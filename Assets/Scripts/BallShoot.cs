c9d825fa2c41c9687210721c0524bac4b9c75d25
Tobias Nagel <tn.tobias.nagel@googlemail.com>
2018-06-19 18:10:26 +0200

    inital commit, fps and ball shooting works, cube is bad

---
+++ Assets/Scripts/BallShoot.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallShoot : MonoBehaviour {
+
+    GameObject prefab;
+	
+	void Start () {
+        prefab = Resources.Load("ball") as GameObject;
+	}
+	
+	void Update () {
+	    if (Input.GetMouseButtonDown(0))
+        {
+            GameObject ball = Instantiate(prefab) as GameObject;
+            ball.transform.position = transform.position + Camera.main.transform.forward * 2;
+            Rigidbody rb = ball.GetComponent<Rigidbody>();
+            rb.velocity = Camera.main.transform.forward * 40;
+        }	
+	}
+}
