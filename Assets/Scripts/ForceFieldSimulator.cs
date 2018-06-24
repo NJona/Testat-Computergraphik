7a4cef43a8ab7e6a82603d6070ec9b09bf54a08a
Tobias Nagel <tn.tobias.nagel@googlemail.com>
2018-06-24 03:21:47 +0200

    imported cubemesh from exercise 1

---
+++ Assets/Scripts/ForceFieldSimulator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForceFieldSimulator : MonoBehaviour {
+
+    public GameObject cube;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+    }
+	
+	void OnCollisionEnter(Collision collision) {
+		foreach (ContactPoint contact in collision.contacts) {		
+			if (cube != null)
+			{
+				Vector4 pos = new Vector4(contact.point.x, contact.point.y, contact.point.z, 1);
+				cube.GetComponent<MeshRenderer>().sharedMaterial.SetVector("_WindSourcePosition", pos);
+			}
+		}
+	}
+}
