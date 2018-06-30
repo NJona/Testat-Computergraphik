44de3638f00c23535e1413b6a359e1f1767f5316
janik2102 <janik.siller@googlemail.com>
2018-06-30 14:44:15 +0200

    Add Excercise 1 to Main Menu

---
+++ A0_GameMenu/Assets/A1_Deformation/Scripts/BallShoot.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class BallShoot : MonoBehaviour {
+
+    GameObject ball;
+	
+	void Start () {
+        ball = Resources.Load("ball") as GameObject;
+	}
+	
+	void Update () {
+	    if (Input.GetMouseButtonDown(0))
+        {
+            GameObject ball = Instantiate(this.ball) as GameObject;
+            ball.transform.position = transform.position + Camera.main.transform.forward * 2;
+            Rigidbody rb = ball.GetComponent<Rigidbody>();
+            rb.velocity = Camera.main.transform.forward * 40;
+        }	
+	}
+}
