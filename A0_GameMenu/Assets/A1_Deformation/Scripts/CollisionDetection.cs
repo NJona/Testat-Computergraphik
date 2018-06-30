157913ab79f31a3aaee5f8f5ede89c0fba83a829
Jona <Jona.Neef.97@gmail.com>
2018-06-30 16:03:15 +0200

    add Deformation Shader and CollisionDetection Script

---
+++ A0_GameMenu/Assets/A1_Deformation/Scripts/CollisionDetection.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+
+namespace Assets.A1_Deformation.Scripts
+{
+    public class CollisionDetection : MonoBehaviour
+    {
+
+        public int DeformationTimeToLive;
+        public int MaxNumberOfDeformations;
+        public float ForceMultiplier;
+        private float[] _timeToLiveArray;
+        private Vector4[] _positionArray;
+
+        public void OnCollisionEnter(Collision collision)
+        {
+            //get Deformation position
+            var position = new Vector4(collision.gameObject.transform.position.x,
+                collision.gameObject.transform.position.y, collision.gameObject.transform.position.z, 1);
+
+            //move last deformation to one index earlier (newest deformation is at index MaxNumberDeformations - 1)
+            for (var i = 0; i < MaxNumberOfDeformations - 1; i++)
+            {
+                _timeToLiveArray[i] = _timeToLiveArray[i + 1];
+                _positionArray[i] = _positionArray[i + 1];
+            }
+
+            //add newest deformation at last array index
+            _timeToLiveArray[MaxNumberOfDeformations - 1] = DeformationTimeToLive;
+            _positionArray[MaxNumberOfDeformations - 1] = position;
+
+            GetComponent<MeshRenderer>().material.SetFloatArray("_deformationTimeToLiveList", _timeToLiveArray);
+            GetComponent<MeshRenderer>().material.SetVectorArray("_collisionPositionList", _positionArray);
+        }
+
+        public void Start()
+        {
+            //check MaxNumberOfDeformations and assign 100 if number is out of shader range (shader range is hard coded to 100)
+            if (MaxNumberOfDeformations > 100 || MaxNumberOfDeformations <= 0)
+            {
+                Debug.Log("MaxNumerOfDeformation not specified or bigger than 100. Use default value '100' instead.");
+                MaxNumberOfDeformations = 100; //100 is Max Array Count in Shader
+            }
+
+            if (DeformationTimeToLive == 0)
+            {
+                Debug.Log("DeformationTimeToLive not specified. Use default value '100' instead");
+                DeformationTimeToLive = 100;
+            }
+
+            //check if ForceMultiplier is too small and asign 1 if
+            if (ForceMultiplier < 0.1f)
+            {
+                Debug.Log(
+                    "ForceMultiplier is less than '0.1'. Please assign Values bigger than '0.1'. Use default value '1' instead");
+                ForceMultiplier = 1;
+            }
+
+            _timeToLiveArray = new float[MaxNumberOfDeformations];
+            _positionArray = new Vector4[MaxNumberOfDeformations];
+
+            //initialize Array with default values
+            for (var i = 0; i < MaxNumberOfDeformations; i++)
+            {
+                _timeToLiveArray[i] = 0;
+                _positionArray[i] = new Vector4(0, 0, 0, 0);
+            }
+
+            GetComponent<MeshRenderer>().material.SetFloatArray("_deformationTimeToLiveList", _timeToLiveArray);
+            GetComponent<MeshRenderer>().material.SetVectorArray("_collisionPositionList", _positionArray);
+            GetComponent<MeshRenderer>().material.SetFloat("_deformationTimeToLive", DeformationTimeToLive);
+            GetComponent<MeshRenderer>().material.SetFloat("_forceMultiplier", ForceMultiplier);
+        }
+
+        public void Update()
+        {
+            for (var i = 0; i < _timeToLiveArray.Length; i++)
+            {
+                //skip time to live, if 0
+                if (!(_timeToLiveArray[i] > 0))
+                {
+                    continue;
+                }
+
+                //decrement time to live, to make deformation smaller
+                _timeToLiveArray[i] = _timeToLiveArray[i] - 1f;
+                GetComponent<MeshRenderer>().material.SetFloatArray("_deformationTimeToLiveList", _timeToLiveArray);
+            }
+        }
+    }
+}
---
+++ A0_GameMenu/Assets/A1_Deformation/Shaders/Deformation.shader
@@ -0,0 +1,73 @@
+﻿Shader "Computergraphics/Deformation" {
+	Properties
+	{
+		_ColorRed("RGB Color Red", Range(0,255)) = 255
+		_ColorGreen("RGB Color Green", Range(0,255)) = 0
+		_ColorBlue("RGB Color Blue", Range(0,255)) = 0
+	}
+SubShader {
+    Pass {
+        CGPROGRAM
+        #pragma vertex vert
+        #pragma fragment frag
+        #include "UnityCG.cginc"
+
+        struct appdata {
+			float2 uv : TEXCOORD0;
+            float4 vertex : POSITION;
+			float3 normal : NORMAL;
+        };
+
+        struct v2f {
+			float2 uv : TEXCOORD0;
+            float4 position : SV_POSITION;
+			fixed4 color : COLOR;
+        };
+
+		float4 _collisionPositionList[100];
+		float _deformationTimeToLiveList[100];
+		float _deformationTimeToLive;
+		float _forceMultiplier;
+
+		float _ColorRed;
+		float _ColorGreen;
+		float _ColorBlue;
+
+
+        v2f vert (appdata v) {
+            v2f o;
+
+			float4 worldVertex = mul(unity_ObjectToWorld, v.vertex);
+
+			float4 force = float4(0,0,0,0);
+
+			//calculate force for each collision point and add to force sum
+			for(int i=0;i<100;i++){
+					float4 collisionDirection = worldVertex - _collisionPositionList[i];
+					float distance = length(collisionDirection);
+
+					float singleDeformationForce = (1 / distance) * (_deformationTimeToLiveList[i] / _deformationTimeToLive); 
+
+					force = force + (_forceMultiplier * singleDeformationForce * collisionDirection);
+			}
+
+			float4 newWorldVertex = worldVertex + force;
+			
+			o.position = mul(UNITY_MATRIX_VP, newWorldVertex);
+
+			o.color.x = v.normal * 0.5 + (_ColorRed / 255);
+			o.color.y = v.normal * 0.5 + (_ColorGreen / 255);
+			o.color.z = v.normal * 0.5 + (_ColorBlue / 255);
+			o.color.w = 1.0;
+
+            return o;
+        }
+
+        fixed4 frag (v2f i) : SV_Target 
+		{
+			return i.color;
+		}
+        ENDCG
+    }
+}
+}
